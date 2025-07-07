58ede0b2030bbdc7a771ac9822fed042a7c162ce
MukamaJ-2 <145841039+MukamaJ-2@users.noreply.github.com>
2025-07-07 21:15:02 +0300

    Update Task.js

--- src/Task.js
+++ src/Task.js
@@ -3,7 +3,7 @@ export const Task = (props) => {
         <div className="task"
         style={{backgroundColor: props.completed ? "orange" : "white"}}>
             <h1>{props.taskName}</h1>
-            <button onClick={()=> props.completeTask(props.id)}>Complete</button>
+            <button onClick={()=> props.completeTask(props.id)}>Done</button>
             <button onClick ={() => props.deleteTask(props.id)}>X</button>
         </div>
       );
